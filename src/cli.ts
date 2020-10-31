bbc1e2b4dd53140be17f1a83b38be307f11355c0
Vlada Rau <vlada_rau@mckinsey.com>
2020-10-31 15:26:45 +0000

    added test counter config

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,25 @@
 #!/usr/bin/env -S node --expose-gc
 
 import * as path from "path";
+import * as parseArgs from "minimist";
 import { run } from "jest";
 
 const config = {
   testSequencer: path.join(__dirname, "sequencer.js"),
 };
 
-let args = [...process.argv.slice(2)];
+let argv = parseArgs(process.argv.slice(2));
+if (argv.cnt) {
+  process.env.cnt = argv.cnt;
+  delete argv.cnt;
+}
+
+let args: string[] = [];
+Object.entries<unknown>(argv).forEach(([key, value]) => {
+  args.push(key);
+  args.push(String(value));
+});
+
 args.push("--config", JSON.stringify(config));
 args.push("--runInBand");
 args.push("--logHeapUsage");
